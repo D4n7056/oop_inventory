6e4b606e44ac5585f39621b3e30c7434ac94fe22
D4n7056 <A07107056@tec.mx>
2025-06-10 21:58:24 -0600

    Update Artwork.h

--- Artwork.h
+++ Artwork.h
@@ -1,24 +1,36 @@
-#include <string>
+#include <iostream>
 #include <vector>
 #include "Artist.h" 
 
 #ifndef ARTWORK_H
 #define ARTWORK_H
 
+using namespace std;
+
 class Artwork {
     private:
         string id, title, type, dimensions, status, location, description;
-        string creationYear, acquisitionDate;
-        float salePrice = 0.0;
-        Artist* artist = nullptr;
-        vector<std::string> materials;
+        int year;
+        float price;
+        // here we define the author of the artwork
+        Artist* artist;
+        // we assign the materials used in the artwork
+        vector<string> materials;
 
     public:
+        Artwork(string _id, string _title, string _type, string _dimensions, string _status, string _location, 
+            string _description, int _year, float _price, Artist* _artist, vector<string> _materials);
         void markAsSold(float finalPrice);
         void updatePrice(float newPrice);
         void changeStatus(const string& newStatus);
         void changeLocation(const string& newLocation);
         string showDetails() const;
+        
+        string getTitle();
+        Artist* getArtist() const;
+
+        // we use the operator == to compare if two artworks are the same
+        bool operator ==(const Artwork& other) const;
 };
 
 #endif
