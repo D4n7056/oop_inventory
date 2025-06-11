e4374b62a66f8cffde374ffa140222ba1e134dbf
D4n7056 <A07107056@tec.mx>
2025-06-10 21:58:44 -0600

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,12 +1,15 @@
 #include "Product.h"
 
+using namespace std;
+
 #ifndef BUTTON_H
 #define BUTTON_H
 
+// we use inheritance to create a subclass of Product 
 class Button : public Product {
 public:
-    Button(string id, string name, float price, int stock);
-    string showInfo() const override;
+    Button(std::string id, std::string name, std::string description, float price, int stock);
+    std::string showInfo() const override;
 };
 
 #endif
