9b72f5f1e2f396c8b8f36f5dc5c9cc98ea536dee
D4n7056 <A07107056@tec.mx>
2025-06-10 21:59:04 -0600

    Update Buyer.h

--- Buyer.h
+++ Buyer.h
@@ -1,27 +1,29 @@
-
 #include <string>
 #include <vector>
 #include <map>
 #include "Artwork.h"
 
+#ifndef BUYER_H
+#define BUYER_H
 
-#ifndef CUSTOMER_H
-#define CUSTOMER_H
-
+using namespace std;
 
 class Buyer {
-private:
-    string firstName, lastName, address, notes;
-    map<std::string, std::string> contact;
-    vector<Artwork*> purchasedArtworks;
 
-public:
-    Buyer(string fn, string ln, string addr);
+    private:
+        string firstName, lastName, address, notes;
+        map<std::string, std::string> contact;
+        vector<Artwork*> purchasedArtworks;
 
-    void addArtwork(Artwork* artwork);
-    void updateContact(const string& type, const string& value);
-    void showPurchaseHistory() const;
-    std::string getFullName() const;
+    public:
+        Buyer(string _firstName, string _lastName, string _address, string _note);
+        // we use method overloading to allow adding either a single artwork or multiple artworks at once
+        void addPurchasedArtwork(Artwork* artwork);
+        void addPurchasedArtwork(vector<Artwork*> artwork);
+        // here we use map as we do it in Artist 
+        void updateContact(const string& type, const string& value);
+        string showPurchaseHistory() const;
+        std::string getFullName() const;
 };
 
 
