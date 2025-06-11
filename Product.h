8f6412681351edab2710f3c2a242525012b82177
D4n7056 <A07107056@tec.mx>
2025-06-10 22:00:08 -0600

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,5 +1,3 @@
-
-#include <string>
 #include <iostream>
 
 using namespace std;
@@ -9,17 +7,19 @@ using namespace std;
 
 
 class Product {
-protected:
-    string id, name, type, description;
-    float price;
-    int stock;
+    protected:
+        string id, name, type, description;
+        float price;
+        int stock;
 
-public:
-    Product(string id, string name, string type, float price, int stock);
+    public:
+        Product(string id, string name, string type, string _description, float price, int stock);
+        // here we declare the pure virtual method
+        // we dont define the method in the cpp
+        virtual string showInfo() const = 0;
 
-    virtual string showInfo() const;
-    void updateStock(int quantity);
-    void changePrice(float newPrice);
+        void updateStock(int quantity);
+        void changePrice(float newPrice);
 };
 
 #endif
