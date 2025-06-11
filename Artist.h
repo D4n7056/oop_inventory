d4d39f4931e3c00932be4e1b90fcf3189ed7f615
D4n7056 <A07107056@tec.mx>
2025-06-10 21:58:06 -0600

    Update Artist.h

--- Artist.h
+++ Artist.h
@@ -7,14 +7,19 @@ using namespace std;
 
 class Artist {
     private:
-        string firstName, lastName, nationality, mainStyle, biography;
-        string birthDate;
+        string firstName, lastName, birthDate, nationality, mainStyle, biography;
+        // map is used to assign pairs of related values for example "email -> artist_email.com"
         map<string, string> contact;
 
     public:
-        Artist(string fn, string ln, string birth, string nat, string style, string bio);
+        Artist(string firstName, string lastName, string birthDay, string nationality, string style, string biography);
         string getFullName() const;
+        // we use the type of the contact(email, instagram...) and the value of it
         void updateContact(const string& type, const string& value);
+        // we use this method to change the principal style of the artist
+        void setMainStyle(const string& style);
+        // we use this method to get a specific type of contact of the artist
+        string getContact(const string& type) const;
 };
 
 #endif
